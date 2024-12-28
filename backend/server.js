9d97f04abdc1e5b5d30f8176536476ddf0ed489b
kaveeshagim <kaveeshagw123456@gmail.com>
2024-12-28 20:48:21 +0530

    mongoose config minor changes

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,6 @@ const PORT = process.env.PORT || 5000;
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error("Database connection failed:", err));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
